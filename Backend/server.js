559361eace1a7d825e3a84673ee7236d2c71546f
Vladislav <vladislav.boiko@tptlive.ee>
2026-03-25 19:47:33 +0200

    starting project

---
+++ Backend/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const gameRoutes = require('./router/GamesRoutes');
+
+app.get("/", (req, res) => {
+    res.send("Welcome to the GameRate API!");
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on port ${PORT}`);
+});
