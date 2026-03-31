c29255827f24a5c74157f20c1b94b7f42d992a51
Vladislav <vladislav.boiko@tptlive.ee>
2026-03-31 10:03:06 +0300

    another fix

--- Backend/server.js
+++ Backend/server.js
@@ -7,13 +7,13 @@ const PORT = process.env.PORT || 3000;
 const gameRoutes = require('./router/GamesRoutes');
 
 app.use(cors());
-app.use(express.static(path.join(__dirname, "..", "Frontend")))
-app.use(express.static(path.join(__dirname, "..", "Frontend", "scripts")))
+app.use(express.static(path.resolve(__dirname, "../Frontend")))
+app.use(express.static(path.resolve(__dirname, "../Frontend/scripts")))
 app.use("/games", gameRoutes);
 
 app.get("/", async (req, res) => {
     try {
-        res.sendFile(path.join(__dirname, "..", "Frontend", "mainPage", "mainPage.html"))
+        res.sendFile(path.resolve(__dirname, "../Frontend/mainPage/mainPage.html"))
     } catch (e) {
         res.status(500).json({ error: e.message });
         console.error(e);
